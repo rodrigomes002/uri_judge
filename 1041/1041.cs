e5444c8344dc777cc416cce4e0bb7f252b8142c5
Rodrigo Gomes Calixto da Silva <rodrigo.gomes@gmail.com>
2022-06-02 23:16:51 -0300

    Add project files.

---
+++ 1041/1041.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri_judge._1041
+{
+    class _1041
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            decimal x = Convert.ToDecimal(entrada[0]);
+            decimal y = Convert.ToDecimal(entrada[1]);
+
+            if (x == 0 && y == 0) Console.WriteLine("Origem");
+            if (x > 0 && y > 0) Console.WriteLine("Q1");
+            if (x < 0 && y > 0) Console.WriteLine("Q2");
+            if (x < 0 && y < 0) Console.WriteLine("Q3");
+            if (x > 0 && y < 0) Console.WriteLine("Q4");
+            if (x > 0 && y == 0) Console.WriteLine("Eixo X");
+            if (x < 0 && y == 0) Console.WriteLine("Eixo X");
+            if (x == 0 && y > 0) Console.WriteLine("Eixo Y");
+            if (x == 0 && y < 0) Console.WriteLine("Eixo Y");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace uri_judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
